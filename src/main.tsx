975a23882e4c461f98c34b47472172bcafd10b78
Bruno Rezende <brunoc.rez@gmail.com>
2024-10-11 10:27:47 -0300

    chore: add toast

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import { PlayerContextProvider } from './contexts/PlayerContext.tsx'
 import { MatchRoom } from './pages/MatchRoom.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { GameContextProvider } from './contexts/GameContext.tsx'
+import { Toaster } from './components/ui/toaster.tsx'
 
 const client = new QueryClient()
 
@@ -37,6 +38,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster />
     <QueryClientProvider client={client}>
       <GameContextProvider>
         <PlayerContextProvider>
