de28d87c5c0bec90ddc71182b390fbd46e60cefc
Bruno Rezende <brunoc.rez@gmail.com>
2024-12-05 13:34:43 -0300

    refactor: lower opacity when calculating score

--- src/components/CalculateScore.tsx
+++ src/components/CalculateScore.tsx
@@ -1,6 +1,6 @@
 export function CalculateScore() {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center z-50 bg-slate-200 opacity-95">
+    <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center z-50 bg-slate-200 opacity-70">
       <h2 style={{ fontFamily: "'Press Start 2P'", fontSize: '12px' }}>
         calculando pontuações
         <span className="dot ml-2"></span>
